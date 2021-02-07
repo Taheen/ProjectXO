6f82e66ec7e177916cf046f13bfee8d500274058
Dennis T <dennistorhov@gmail.com>
2021-02-07 16:31:09 +0000

    Create FieldTest.java
    adding tests for classes Field, Player.

---
+++ tests/hexlet/xo/model/FieldTest.java
@@ -0,0 +1,29 @@
+package hexlet.xo.model;
+
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FieldTest {
+
+    @Test
+    public void testGetSize() {
+        final Field field = new Field();
+
+        assertEquals(3 ,field.getSize());
+    }
+
+    @Test
+    public void testSetFigure() {
+        final Field field = new Field();
+        final Point inputPoint = new Point(0 , 0);
+        final Figure inputFigure = Figure.O;
+
+        field.setFigure(inputPoint, inputFigure);
+        final Figure actualFigure = field.getFigure(inputPoint);
+
+        assertEquals(inputFigure, actualFigure);
+    }
+}
